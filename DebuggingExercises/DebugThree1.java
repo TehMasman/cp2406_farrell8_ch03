18aa06fb2251f4c80891ecde35da237b03cdd51e
Sam Kahsnitz <samkahsnitz@gmail.com>
2017-08-17 12:22:45 +1000

    debug

--- DebuggingExercises/DebugThree1.java
+++ DebuggingExercises/DebugThree1.java
@@ -1,18 +1,18 @@
 // This class calculates a waitperson's tip as 15% of the bill
 public class DebugThree1
 {
-   public static void main(String args[])
-   {
-      double myCheck = 50.00;
-      double yourCheck = 19.95;
-      System.out.println("Tips are");
-      calcTip(mycheck);
-      calctip(yourCheck);
-    }
-    public static int calcTip(int bill)
+    public static void main(String args[])
     {
-      final double RATE = 0.15;
-      tip = bill + RATE;
-      System.out.println("The tip should be at least " + tip);
+        double myCheck = 50.00;
+        double yourCheck = 19.95;
+        System.out.println("Tips are");
+        calcTip(myCheck);
+        calcTip(yourCheck);
+    }
+    public static void calcTip(double bill)
+    {
+        final double RATE = 0.15;
+        final double tip = bill * RATE;
+        System.out.println("The tip should be at least " + tip);
     }
 }
--- DebuggingExercises/DebugThree3.java
+++ DebuggingExercises/DebugThree3.java
@@ -6,17 +6,18 @@ public class DebugThree3
    {
       String name;
       getName();
-      displayGreeting(name);           
+
    }
    public static String getName()
    {
       String name;
       Scanner input = new Scanner(System.in);
-      System.out.print(Enter name );
-      name = input.nextInt();
+      System.out.print("Enter name ");
+      name = input.next();
+      displayGreeting(name);
       return name;
    }
-   public static void displayGreeting()
+   public static void displayGreeting(String name)
    {
       System.out.println("Hello, " + name + "!");
    }
